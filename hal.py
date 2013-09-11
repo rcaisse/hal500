200cbbb064bdfa0aba65831e1ac74d5dc2a820d7
Ruth Caisse <ruthbca@gmail.com>
2013-09-10 23:47:50 -0400

    Initial Commit

---
+++ hal.py
@@ -0,0 +1,6 @@
+print "Hello, my name is Hal. Welcome!"
+name = raw_input("Please, tell me your name.")
+title = raw_input("What do you like to be called?")
+print "Hi %s. I will not call you %s because I am an asshole." % (name, title)
+print "Just kidding, %s." % title
+mood = raw_input("How are you?")
