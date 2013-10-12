a0f6294f53266df3d71e070fe87a0314deacdd22
Ruth Caisse <ruthbca@gmail.com>
2013-10-12 11:44:37 -0400

    Added mood functionality

--- hal.py
+++ hal.py
@@ -1,6 +1,19 @@
 print "Hello, my name is Hal. Welcome!"
-name = raw_input("Please, tell me your name.")
-title = raw_input("What do you like to be called?")
+name = raw_input("Please, tell me your name. ")
+title = raw_input("What do you like to be called? ")
 print "Hi %s. I will not call you %s because I am an asshole." % (name, title)
 print "Just kidding, %s." % title
-mood = raw_input("How are you?")
+
+mood = raw_input("How are you? ")
+mood = mood.lower()
+
+is_happy = False
+happy_options = ["good", "not bad", "ok", "okay", "not too bad"]
+for option in happy_options:
+	if option in mood: 
+		is_happy = True
+
+if is_happy: 
+	print "I am glad to hear that! I am in a very good mood today."
+else:
+	print "Oh... Well, go outside and do something fun!"
